67791222913c276aaa3f1fb0cae57108da28e393
DANIEL-PC\Administrator <Danarboli2@gmail.com>
2024-09-25 21:17:44 +0200

    no message

---
+++ Ejercicio 1/src/EjercicioPrueba1.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class EjercicioPrueba1 {
+    public void EjercicoPrueba1() {
+
+        System.out.println("¿Como te llamas?");
+
+        Scanner entrada = new Scanner(System.in);
+        //System.in
+
+        String nombre = entrada.next();
+
+        System.out.println("¿A qué curso");
+        String curso = entrada.next();
+
+
+        System.out.println("Hola " + nombre + " Bienvenido a " + curso);
+
+        int num1 = 0;
+        int num2 = 0;
+
+        while (num1 == num2) {
+            System.out.println("Introduce un numero...");
+            num1 = entrada.nextInt();
+
+            System.out.println("Introduce un numero...");
+            num2 = entrada.nextInt();
+
+            if (num1 == num2) {
+
+            }
+
+            int resultado = num1 + num2;
+
+            System.out.println("El resultado es" + resultado);
+            num2 = entrada.nextInt();
+
+            if (num1 != num2) {
+                resultado = num1 + num2;
+                System.out.println("El resultado del la suma es" + resultado);
+
+            } else {
+                //pedir otra vez
+            }
+        }
+    }}
